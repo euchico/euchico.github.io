f7139882f18b4005dce70f4a06df66f23fdeda54
euchico <franciscodepaula.web@gmail.com>
2020-03-07 23:03:06 -0300

    Recomeço do desenvolvimento do projeto

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,23 @@
 $('document').ready(function(){
-  switch ($('body').attr('id')) {
-    // HOME --------------------------------------------------
-    case ("home"):
-      var texto1 = new Typed('#titleMain', {
-        strings: ['Francisco de Paula'],
-        typeSpeed: 40,
-        backSpeed: 0,
-        loop: false,
-        showCursor: false
-      });
 
-      var texto2 = new Typed('#titleQuote', {
-        strings: ['Desenhista?',
-                  'Professor de Matemática?',
-                  '<strong>Desenvolvedor Web!</strong> ^5000'],
-        startDelay: 2000,
-        typeSpeed: 40,
-        backSpeed: 10,
-        loop: true,
-        fadeOut: true,
-        showCursor: false
-      });
-      break;
+  var texto1 = new Typed('#titleMain', {
+    strings: ['Francisco de Paula'],
+    typeSpeed: 40,
+    backSpeed: 0,
+    loop: false,
+    showCursor: false
+  });
 
-    // CURRICULUM --------------------------------------------------
-    case ("curriculum"):
-      $('[data-toggle="collapse"]').click(function(){
-      	$("div > div > .glyphicon",this).toggleClass("fa-angle-down fa-angle-up")
-      })
-    break;
-  }
+  var texto2 = new Typed('#titleQuote', {
+    strings: ['<strong>Programação</strong> ^1000',
+              '<strong>Matemática</strong> ^1000',
+              '<strong>Arte</strong> ^1000'],
+    startDelay: 2000,
+    typeSpeed: 40,
+    backSpeed: 10,
+    loop: true,
+    fadeOut: true,
+    showCursor: false
+  });
+
 });
