2a79ece2f356ebd2cf8fe570f1bce9d4f9a2aba1
Francisco de Paula <eu.francisco@outlook.com>
2021-08-12 22:44:01 -0300

    Remoçao do jQuery e atualizaçao de funçao

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
-// $('document').ready(function(){
-//   $('[data-toggle="collapse"]').click(function(){
-//     $("div > div > .glyphicon",this).toggleClass("fa-angle-down fa-angle-up")
-//   })
-// });
+function toggleArrow(id){
+  var el = document.getElementById(id);
+
+  if (el.classList.contains("fa-angle-down")){
+    el.classList.remove("fa-angle-down");
+    el.classList.add("fa-angle-up");
+  }else{
+    el.classList.remove("fa-angle-up");
+    el.classList.add("fa-angle-down");
+  }
+}
